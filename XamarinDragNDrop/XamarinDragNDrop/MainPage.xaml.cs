99d987266d584a530c6c64ff902e4424bc8a732a
David Morrow <quickbasic44@outlook.com>
2022-05-09 21:11:38 -0400

    Add project files.

---
+++ XamarinDragNDrop/XamarinDragNDrop/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinDragNDrop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            var label = (sender as Element).Parent as Label;
+
+
+        }
+
+        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+        {
+            DropZone.Text = e.Data.GetTextAsync().Result;
+
+        }
+
+        private void DropGestureRecognizer_Drop_1(object sender, DropEventArgs e)
+        {
+            DropZone2.Text = e.Data.GetTextAsync().Result;
+        }
+
+        private void DragGestureRecognizer_DragStarting_1(object sender, DragStartingEventArgs e)
+        {
+
+        }
+    }
+}
